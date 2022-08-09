e02786f4405649319153872b8cd24ab7c02e4daa
GRAPECITY\ShaySong <Shay.Song@grapecity.com>
2022-08-09 16:14:56 +0800

    修改list

--- CSharp/IList/IList/Program.cs
+++ CSharp/IList/IList/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 
-namespace list{
+namespace practice
+{
+
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.print();
+        }
+    }
+
     class Node<T>
     {
         public T _Value;
-        public Node<T> _Next;
-        public Node() : this(default(T)) { }
+        public Node<T>? _Next;
         public Node(T value)
         {
             _Value = value;
@@ -18,15 +32,171 @@ namespace list{
         }
     }
 
-    public class IList<T>
+    public class List<T>
     {
-        private Node<T> head = null;
+        private Node<T>? head = null;
         private int _count = 0;
-        public int Count { get
+
+        public int Count
+        {
+            get
             {
                 return _count;
             }
         }
+        public bool IsFixedSize
+        {
+            get
+            {
+                return true;
+            }
+        }
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        /**
+         * @brief 添加
+         * @param item 基础类型
+         */
+        public void Add(T item)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(item);
+                _count++;
+            }
+            else
+            {
+                Node<T> node = head;
+                while (node._Next != null)
+                {
+                    node = node._Next;
+                    _count++;
+                }
+                node._Next = new Node<T>(item);
+            }
+        }
+        /**
+         * @brief 插入
+         * @param index 位置
+         * @param item 基础类型
+         */
+        public void Insert(int index, T item)
+        {
+            if (index > _count)
+            {
+                throw new Exception("Error! insert out of range");
+            }
+            Node<T> node = head;
+            Node<T> pre = null;
+            Node<T> next;
+            if (index == 0)
+            {
+                next = head;
+                head = new Node<T>(item);
+                head._Next = next;
+            }
+            else
+            {
+                for (int i = 0; i < index; i++)
+                {
+                    pre = node;
+                    node = node._Next;
+                }
+                next = node;
+                node = new Node<T>(item);
+                node._Next = next;
+                pre._Next = node;
+            }
+            _count++;
+        }
+
+        /**
+         * @brief 是否包含
+         * @param item 基础类型
+         */
+        public bool Contains(T item)
+        {
+            return IndexOf(item) != -1 ? true : false;
+        }
+        /**
+         * @brief 下标位置
+         * @param item 基础类型
+         */
+        public int IndexOf(T item)
+        {
+            int result = -1;
+            Node<T> node = head;
+            for (int i = 0; i < _count; i++)
+            {
+                if (node._Value.Equals(item))
+                {
+                    result = i;
+                    break;
+                }
+                node = node._Next;
+            }
+            return result;
+        }
+
+        /**
+         * @brief 清空
+         */
+        public void Clear()
+        {
+            head = null;
+            _count = 0;
+        }
+        /**
+         * @brief 删除
+         * @param item 基础类型
+         */
+        public bool Remove(T item)
+        {
+            int idx = IndexOf(item);
+            if (idx == -1)
+            {
+                return false;
+            }
+            RemoveAt(idx);
+            return true;
+        }
+        /**
+         * @brief 删除
+         * @param index 位置
+         */
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new Exception("Error! remvoe out of range");
+            }
+            Node<T> node = head;
+            if (index == 0)
+            {
+                head = head._Next;
+            }
+            else
+            {
+                Node<T> pre = null;
+                for (int i = 0; i <= index; i++)
+                {
+                    pre = node;
+                    node = node._Next;
+                }
+                pre._Next = node._Next;
+            }
+            _count--;
+        }
+
+        /**
+         * @brief 索引器
+         */
         public T this[int index]
         {
             get
@@ -47,142 +217,31 @@ namespace list{
                 Insert(index, value);
             }
         }
-        public void Add(T item)
-        {
-            if (head == null)
-            {
-                head = new Node<T>(item);
-                _count++;
-            }
-            else
-            {
-                Node<T> node = head;
-                while(node._Next != null)
-                {
-                    node = node._Next;
-                    _count++;
-                }
-                node._Next = new Node<T>(item);
-            }
-        }
-
-        public void Clear()
-        {
-            head = null;
-            _count = 0;
-        } 
-
-        public bool Contains(T item)
-        {
-            return IndexOf(item) != -1 ? true : false;
-        }
-
-        public bool Remove(T item)
-        {
-            int idx = IndexOf(item);
-            if(idx == -1)
-            {
-                return false;
-            }
-            RemoveAt(idx);
-            return true;
-        }
-
-        public int IndexOf(T item)
-        {
-            int result = -1;
-            Node<T> node = head;
-            for (int i = 0; i < _count; i++)
-            {
-                if (node._Value.Equals(item))
-                {
-                    result = i;
-                    break;
-                }
-                node = node._Next;
-            }
-            return result;
-        }
-
-        public void Insert(int index, T item)
-        {
-            if ((index == 0 && _count == 0) || (index >= 0 && index < _count))
-            {
-                Node<T> node = head;
-                Node<T> pre = null;
-                Node<T> next = null;
-                if (index == 0)
-                {
-                    next = head;
-                    head = new Node<T>(item);
-                    head._Next = next;
-                }
-                else
-                {
-                    for (int i = 0; i < index; i++)
-                    {
-                        pre = node;
-                        node = node._Next;
-                    }
-                    next = node;
-                    node = new Node<T>(item);
-                    node._Next = next;
-                    pre._Next = node;
-                }
-                _count++;
-                return;
-            }
-            throw new Exception("Error! insert out of range");
-        }
-
-        public void RemoveAt(int index)
-        {
-            if (index < 0 || index >= _count)
-            {
-                throw new Exception("Error! remvoe out of range");
-            }
-            Node<T> node = head;
-            if (index == 0) 
-            {
-                head = head._Next;
-            }
-            else
-            {
-                Node<T> pre = null;
-                for (int i = 0; i <= index; i++)
-                {
-                    pre = node;
-                    node = node._Next;
-                }
-                pre._Next = node._Next;
-            }
-            _count--;
-        }
-
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            Node<T> node = head;
-            for (int i = 0; i < _count; i++)
-            {
-                array[arrayIndex + i] = node._Value;
-                node = node._Next;
-            }
-        }
-
-        public IEnumerator<T> GetEnumberator()
+        /**
+         * @brief 迭代器
+         */
+        public IEnumerator<T> GetEnumerator()
         {
             Node<T> node = head;
             Node<T> result;
-            while(node != null)
+            while (node != null)
             {
                 result = node;
                 node = node._Next;
                 yield return result._Value;
             }
         }
-        IEnumerator IEnumerable.GetEnumerator()
+        /**
+         * @brief 打印全部元素
+         */
+        public void print()
         {
-            throw new NotImplementedException();
+            Node<T> node = head;
+            while (node != null)
+            {
+                Console.WriteLine(node._Value);
+                node = node._Next;
+            }
         }
     }
 }
--- CSharp/IList/IListTests/IListTests.cs
+++ CSharp/IList/IListTests/IListTests.cs
@@ -1,4 +1,4 @@
-using list;
+using practice;
 
 namespace IListTests
 {
@@ -8,45 +8,67 @@ namespace IListTests
         [TestMethod]
         public void Add_WithValidIndex()
         {
-            list.IList<int> list = new list.IList<int>();
+            practice.List<int> list = new practice.List<int>();
             list.Add(1);
             list.Add(-1);
             list.Add('a');
             list.Add(999999);
+            list.print();
         }
 
         [TestMethod]
-        public void Remove_WithValidIndex()
+        public void Remove_WithValidNum()
         {
-            list.IList<int> list = new list.IList<int>();
+            practice.List<int> list = new practice.List<int>();
             list.Add(1);
             list.Add(2);
             list.Add(2);
             list.Add(3);
+            list.Add(4);
 
-            list.Remove(2);
-            list.Remove(1);
-            list.Remove(2);
-            list.Remove(2);
+            Console.WriteLine(list.Remove(2));
+            Console.WriteLine(list.Remove(1));
+            Console.WriteLine(list.Remove(2));
+            Console.WriteLine(list.Remove(2));
+            list.print();
         }
+
         [TestMethod]
-        public void Indexer_WithValidIndex()
+        public void Insert_WithValidIndex()
         {
-            list.IList<int> list = new list.IList<int>();
+            practice.List<int> list = new practice.List<int>();
             list.Insert(0, 1);
             list.Insert(1, 4);
-            list.Insert(4, 5);
-            list.Insert(-1, 'c');
+
+            //list.Insert(4, 5);    //error
+            //list.Insert(-1, 'c'); //error
+            list.print();
         }
 
         [TestMethod]
-        public void IEnumerator_WithValidIndex()
+        public void Indexer_Valid()
         {
-            list.IList<int> list = new list.IList<int>();
+            practice.List<int> list = new practice.List<int>();
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+            list.Add(1);
+
+            list[2] = 2;
+            list[3] = 3;
+            //list[7] = 0;   //error
+            //list[-1] = 0;  //error
+            list.print();
+        }
+
+        [TestMethod]
+        public void IEnumerator_Valid()
+        {
+            practice.List<int> list = new practice.List<int>();
             list.Add(1);
             list.Add(2);
-            list.Add(2);
             list.Add(3);
+            list.Add(4);
 
             foreach(int num in list) {
                 Console.WriteLine(num);
