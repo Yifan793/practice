8492497d6668189d2310a045dfce1a555424e636
GRAPECITY\ShaySong <Shay.Song@grapecity.com>
2022-08-11 10:28:58 +0800

    构建页面

---
+++ CSharp/Contacts/Contacts/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Contacts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DependencyObject obj = e.OriginalSource as DependencyObject;
+            TreeViewItem item = GetDependencyObjectFromVisualTree(obj, typeof(TreeViewItem)) as TreeViewItem;
+            if (item == null)
+            {
+                return;
+            }
+            Console.WriteLine(typeof(GroupNodeItem));
+            Console.WriteLine(item.Header.GetType());
+            return;
+            GroupNodeItem groupItem = (GroupNodeItem)item.Header;
+            if (groupItem != null)
+            {
+                return;
+            }
+
+            PersonNodeItem personItem = (PersonNodeItem)item.Header;
+            if (personItem != null)
+            {
+                return;
+            }
+
+        }
+        private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DependencyObject obj = e.OriginalSource as DependencyObject;
+            TreeViewItem item = GetDependencyObjectFromVisualTree(obj, typeof(TreeViewItem)) as TreeViewItem;
+            if (item == null)
+            {
+                return;
+            }
+            GroupNodeItem selectedElement = (GroupNodeItem)item.Header;
+            string header = selectedElement.Name;
+            if (header.ToUpper() == "PACKAGES")
+            {
+                // Packages root node
+                MenuItem mnuItem = new MenuItem();
+                mnuItem.Header = "New Package";
+                ContextMenu menu = new ContextMenu() { };
+                menu.Items.Add(mnuItem);
+                (sender as TreeViewItem).ContextMenu = menu;
+            }
+        }
+        private static DependencyObject GetDependencyObjectFromVisualTree(
+            DependencyObject startObject,
+            Type type)
+        {
+            var parent = startObject;
+            while (parent != null)
+            {
+                if (type.IsInstanceOfType(parent))
+                    break;
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return parent;
+        }
+
+        //public void AddGroup_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var item = this.tree1.SelectedItem as NodeX;
+        //    if (item != null)
+        //    {
+        //        MessageBox.Show(item.Name.ToString());
+        //    }
+        //}
+
+        //public void DeleteGroup_Click(object sender, RoutedEventArgs e)
+        //{
+        //    var item = this.tree1.selecteditem as nodex;
+        //    if (item != null)
+        //    {
+        //        MessageBox.show(item.name.tostring());
+        //    }
+        //}
+    }
+}
---
+++ CSharp/Contacts/Contacts/MainWindowViewModel.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows;
+using GalaSoft.MvvmLight;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace Contacts
+{
+    //public class MainWindowViewModel : INotifyPropertyChanged
+    //{
+    //    private string _myString = "Some text from MainWindowViewModel";
+    //    public string MyString
+    //    {
+    //        get { return _myString; }
+    //        set
+    //        {
+    //            _myString = value;
+    //            NotifyPropertyChanged();
+    //        }
+    //    }
+
+    //    public ICommand ChangeText { get; set; }
+    //    public MainWindowViewModel()
+    //    {
+    //        ChangeText = new ChangeTextCommand { mwv = this };
+    //    }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    //    {
+    //        if (PropertyChanged != null)
+    //        {
+    //            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    //        }
+    //    }
+    //}
+
+    public class GroupNodeItem
+    {
+        public GroupNodeItem()
+        {
+            Members = new ObservableCollection<PersonNodeItem>();
+        }
+        public string Name { get; set; }
+        public ObservableCollection<PersonNodeItem> Members { get; set; }
+    }
+
+    public class PersonNodeItem
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public bool Gender { get; set; }
+        public string Birthday { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+    }
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public RelayCommand AddGroupCommand { get; set; }
+        public RelayCommand DeleteGroupCommand { get; set; }
+        public RelayCommand AddPersonComamnd { get; set; }
+        public RelayCommand DeletePersonCommand { get; set; }
+
+        public MainWindowViewModel ()
+        {
+            AddGroupCommand = new RelayCommand(AddGroup);
+        }
+        private void AddGroup()
+        {
+            Groups.Add(new GroupNodeItem()
+            {
+                Name = "test add"
+            });
+            //RaisePropertyChanged("Groups");
+        }
+
+
+        private string myString = "Overtype this text";
+        public string MyString
+        {
+            get { return myString; }
+            set
+            {
+                myString = value;
+                RaisePropertyChanged();  // implementation of INotifyPropertyChanged is from ViewModelBase
+            }
+        }
+
+        public ObservableCollection<GroupNodeItem> Groups { get; set; } = new()
+        {
+            new GroupNodeItem() {
+                Name = "test 1",
+                Members = new ObservableCollection<PersonNodeItem>()
+                {
+                    new PersonNodeItem()
+                    {
+                        Name = "test child_1"
+                    },
+                    new PersonNodeItem() 
+                    { 
+                        Name = "test child_2"
+                    }
+                }
+            }
+        };
+    }
+}
