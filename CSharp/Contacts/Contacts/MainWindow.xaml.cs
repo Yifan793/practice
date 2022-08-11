d39c6cf717ab90324846ebbc852e309ae20f95dd
GRAPECITY\ShaySong <Shay.Song@grapecity.com>
2022-08-11 11:06:48 +0800

    调整代码结构

--- CSharp/Contacts/Contacts/MainWindow.xaml.cs
+++ CSharp/Contacts/Contacts/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Contacts
 {
@@ -24,6 +29,8 @@ namespace Contacts
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
         }
 
         private void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -34,21 +41,14 @@ namespace Contacts
             {
                 return;
             }
-            Console.WriteLine(typeof(GroupNodeItem));
-            Console.WriteLine(item.Header.GetType());
-            return;
-            GroupNodeItem groupItem = (GroupNodeItem)item.Header;
-            if (groupItem != null)
+            if(item.Header is GroupViewModel)
             {
-                return;
+                Console.WriteLine("group");
             }
-
-            PersonNodeItem personItem = (PersonNodeItem)item.Header;
-            if (personItem != null)
+            else if (item.Header is PersonViewModel)
             {
-                return;
+                Console.WriteLine("Person");
             }
-
         }
         private void OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
         {
@@ -58,16 +58,13 @@ namespace Contacts
             {
                 return;
             }
-            GroupNodeItem selectedElement = (GroupNodeItem)item.Header;
-            string header = selectedElement.Name;
-            if (header.ToUpper() == "PACKAGES")
+            if (item.Header is GroupViewModel)
             {
-                // Packages root node
-                MenuItem mnuItem = new MenuItem();
-                mnuItem.Header = "New Package";
-                ContextMenu menu = new ContextMenu() { };
-                menu.Items.Add(mnuItem);
-                (sender as TreeViewItem).ContextMenu = menu;
+                Console.WriteLine("group");
+            }
+            else if (item.Header is PersonViewModel)
+            {
+                Console.WriteLine("Person");
             }
         }
         private static DependencyObject GetDependencyObjectFromVisualTree(
@@ -82,6 +79,11 @@ namespace Contacts
                 parent = VisualTreeHelper.GetParent(parent);
             }
             return parent;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
         }
 
         //public void AddGroup_Click(object sender, RoutedEventArgs e)
@@ -101,5 +103,130 @@ namespace Contacts
         //        MessageBox.show(item.name.tostring());
         //    }
         //}
+    }
+
+    public class GroupViewModel : INotifyPropertyChanged
+    {
+        public GroupViewModel()
+        {
+            Members = new ObservableCollection<PersonViewModel>();
+        }
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged(); } }
+        public ObservableCollection<PersonViewModel> Members { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; OnPropertyChanged(); } }
+        private string _number;
+        public string Number { get { return _number; } set { _number = value; OnPropertyChanged(); } }
+        private bool _gender;
+        public bool Gender { get { return _gender; } set { _gender = value; OnPropertyChanged(); } }
+        private string _birthday;
+        public string Birthday { get { return _birthday; } set { _birthday = value; OnPropertyChanged(); } }
+        private string _avatar;
+        public string Avatar { get { return _avatar; } set { _avatar = value; OnPropertyChanged(); } }
+        private string _email;
+        public string Email { get { return _email; } set { _email = value; OnPropertyChanged(); } }
+        private string _notes;
+        public string Notes { get { return _notes; } set { _notes = value; OnPropertyChanged(); } }
+        public bool Visible { get; set; }
+
+        public Person Model
+        {
+            get
+            {
+                return new Person()
+                {
+                    Name = this.Name,
+                    Number = this.Number,
+                    Gender = this.Gender,
+                    Birthday = this.Birthday,
+                    Avatar = this.Avatar,
+                    Email = this.Email,
+                    Notes = this.Notes,
+                };
+            }
+            set
+            {
+                Name = value.Name;
+                Number = value.Number;
+                Gender = value.Gender;
+                Birthday = value.Birthday;
+                Avatar = value.Avatar;
+                Email = value.Email;
+                Notes = value.Notes;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        public RelayCommand AddGroupCommand { get; set; }
+        public RelayCommand DeleteGroupCommand { get; set; }
+        public RelayCommand AddPersonCommand { get; set; }
+        public RelayCommand DeletePersonCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            AddGroupCommand = new RelayCommand(AddGroup);
+        }
+        private void AddGroup()
+        {
+            Groups.Add(new GroupViewModel()
+            {
+                Name = "test add"
+            });
+        }
+
+        private string myString = "Overtype this text";
+        public string MyString
+        {
+            get { return myString; }
+            set
+            {
+                myString = value;
+                RaisePropertyChanged();  // implementation of INotifyPropertyChanged is from ViewModelBase
+            }
+        }
+
+        public ObservableCollection<GroupViewModel> Groups { get; set; } = new()
+        {
+            new GroupViewModel() {
+                Name = "test 1",
+                Members = new ObservableCollection<PersonViewModel>()
+                {
+                    new PersonViewModel()
+                    {
+                        Name = "test child_1"
+                    },
+                    new PersonViewModel()
+                    {
+                        Name = "test child_2"
+                    }
+                }
+            }
+        };
     }
 }
---
+++ CSharp/Contacts/Contacts/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+        public bool Gender { get; set; }
+        public string Birthday { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string Notes { get; set; }
+    }
+}
