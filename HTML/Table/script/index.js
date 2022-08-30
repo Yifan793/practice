1cabbfa2bc238ecca95667e690ea263318b78687
YIFAN\11466 <1146602243@qq.com>
2022-08-30 11:16:17 +0800

    显示类excel

--- HTML/Table/script/index.js
+++ HTML/Table/script/index.js
@@ -48,7 +48,13 @@ var testData = [
 ];
 function init() {
     var table = new Table();
-    table.createTableFromJSON(testColumns, testData);
+    // table.createTableFromJSON(testColumns, testData);
+    table.createEmptyTable(25, 25);
+    table.insertColumn(1);
+    table.insertColumn(4);
+    table.deleteColumn(2);
+    table.insertRow(1);
+    table.generateDataToView();
 }
 var Column = /** @class */ (function () {
     function Column(name, selector) {
@@ -58,81 +64,115 @@ var Column = /** @class */ (function () {
     return Column;
 }());
 var Cell = /** @class */ (function () {
-    function Cell(row, col, name, value) {
-        this.row = row;
-        this.col = col;
+    function Cell(name, value) {
         this.name = name;
         this.value = value;
     }
     return Cell;
 }());
 var Row = /** @class */ (function () {
-    function Row(id) {
-        this.id = id;
-    }
-    Row.prototype.getValue = function (name) {
-        for (var _i = 0, _a = this.cellList; _i < _a.length; _i++) {
-            var cell = _a[_i];
-            if (cell.name == name) {
-                return cell.value;
-            }
+    function Row(count) {
+        var list = new Array();
+        for (var j = 0; j < count; j++) {
+            var cell = new Cell("", "");
+            list.push(cell);
         }
-        return "";
+        this.cellList = list;
+    }
+    Row.prototype.insertColumn = function (index) {
+        var cell = new Cell("", "");
+        this.cellList.splice(index, 0, cell);
+    };
+    Row.prototype.deleteColumn = function (index) {
+        this.cellList.splice(index, 1);
     };
     return Row;
 }());
 var Table = /** @class */ (function () {
     function Table() {
     }
-    Table.prototype.createTableFromJSON = function (columns, data) {
-        var cols = new Array();
-        for (var _i = 0, columns_1 = columns; _i < columns_1.length; _i++) {
-            var column = columns_1[_i];
-            var col = new Column(column.name, column.selector);
-            cols.push(col);
+    Table.prototype.createEmptyTable = function (col, row) {
+        this.columns = new Array();
+        for (var i = 0; i < col; i++) {
+            var col_1 = new Column("", "");
+            this.columns.push(col_1);
         }
-        var rowList = new Array();
-        for (var i = 0; i < data.length; i++) {
-            var row = new Row(i);
-            var list = new Array();
-            for (var j = 0; j < cols.length; j++) {
-                var cell = new Cell(i, j, cols[j].selector, data[i][cols[j].selector]);
-                list.push(cell);
-            }
-            row.cellList = list;
-            rowList.push(row);
+        this.rows = new Array();
+        for (var i = 0; i < row; i++) {
+            var row_1 = this.generateRow();
+            this.rows.push(row_1);
         }
+    };
+    Table.prototype.generateDataToView = function () {
         var table = document.createElement("div");
+        table.id = "showTable";
         var tr = document.createElement("div");
+        tr.className = "row";
         var idCell = document.createElement("div");
         idCell.innerHTML = "";
         idCell.className = "id";
         tr.appendChild(idCell);
-        for (var i = 1; i < cols.length; i++) {
+        for (var i = 0; i < this.columns.length; i++) {
             var th = document.createElement("div");
-            th.innerHTML = cols[i].name;
+            th.innerHTML = this.numberToName(i + 1);
             th.className = "head";
             tr.appendChild(th);
         }
         table.appendChild(tr);
-        for (var i = 0; i < rowList.length; i++) {
+        for (var i = 0; i < this.rows.length; i++) {
             var tr_1 = document.createElement("div");
+            tr_1.className = "row";
             var idCell_1 = document.createElement("div");
-            idCell_1.innerHTML = rowList[i].id.toString();
+            idCell_1.innerHTML = (i + 1).toString();
             idCell_1.className = "id";
             tr_1.append(idCell_1);
-            for (var j = 1; j < cols.length; j++) {
+            for (var j = 0; j < this.columns.length; j++) {
                 var cell = document.createElement("div");
-                cell.innerHTML = rowList[i].getValue(cols[j].selector);
+                cell.innerHTML = this.rows[i].cellList[j].value;
                 cell.className = "cell";
                 tr_1.appendChild(cell);
             }
             table.appendChild(tr_1);
         }
-        var container = document.getElementById("showTable");
+        var container = document.getElementById("container");
         if (container != null) {
             container.innerHTML = "";
             container.appendChild(table);
+        }
+    };
+    Table.prototype.numberToName = function (num) {
+        var name = "";
+        while (num >= 1) {
+            name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
+            console.log(num);
+            num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+        }
+        return name;
+    };
+    Table.prototype.insertRow = function (index) {
+        var row = this.generateRow();
+        this.rows.splice(index, 0, row);
+    };
+    Table.prototype.deleteRow = function (index) {
+        this.rows.splice(index, 1);
+    };
+    Table.prototype.generateRow = function () {
+        var row = new Row(this.columns.length);
+        return row;
+    };
+    Table.prototype.insertColumn = function (index) {
+        var col = new Column("", "");
+        this.columns.splice(index, 0, col);
+        for (var _i = 0, _a = this.rows; _i < _a.length; _i++) {
+            var row = _a[_i];
+            row.insertColumn(index);
+        }
+    };
+    Table.prototype.deleteColumn = function (index) {
+        this.columns.splice(index, 1);
+        for (var _i = 0, _a = this.rows; _i < _a.length; _i++) {
+            var row = _a[_i];
+            row.deleteColumn(index);
         }
     };
     return Table;
--- HTML/Table/script/index.ts
+++ HTML/Table/script/index.ts
@@ -50,7 +50,13 @@ const testData = [
 
 function init() {
   let table = new Table();
-  table.createTableFromJSON(testColumns, testData);
+  // table.createTableFromJSON(testColumns, testData);
+  table.createEmptyTable(25, 25);
+  table.insertColumn(1);
+  table.insertColumn(4);
+  table.deleteColumn(2);
+  table.insertRow(1);
+  table.generateDataToView();
 }
 
 class Column {
@@ -63,92 +69,142 @@ class Column {
 }
 
 class Cell {
-  constructor(row: number, col: number, name: string, value: string) {
-    this.row = row;
-    this.col = col;
+  constructor(name: string, value: string) {
     this.name = name;
     this.value = value;
   }
-  public row: number;
-  public col: number;
   public name: string;
   public value: string;
 }
 
 class Row {
-  public id: number;
-  constructor(id: number) {
-    this.id = id;
+  constructor(count: number) {
+    let list = new Array<Cell>();
+    for (var j = 0; j < count; j++) {
+      let cell = new Cell("", "");
+      list.push(cell);
+    }
+    this.cellList = list;
   }
   public cellList: Cell[];
-  getValue(name: string) {
-    for (var cell of this.cellList) {
-      if (cell.name == name) {
-        return cell.value;
-      }
-    }
-    return "";
+  insertColumn(index: number) {
+    let cell = new Cell("", "");
+    this.cellList.splice(index, 0, cell);
+  }
+  deleteColumn(index: number) {
+    this.cellList.splice(index, 1);
   }
 }
 
 class Table {
   constructor() {}
   public columns: Array<Column>;
-  createTableFromJSON(columns: any[], data: any) {
-    let cols = new Array<Column>();
-    for (let column of columns) {
-      let col = new Column(column.name, column.selector);
-      cols.push(col);
-    }
+  public rows: Array<Row>;
 
-    let rowList = new Array<Row>();
-    for (var i = 0; i < data.length; i++) {
-      let row = new Row(i);
-      let list = new Array<Cell>();
-      for (var j = 0; j < cols.length; j++) {
-        let cell = new Cell(i, j, cols[j].selector, data[i][cols[j].selector]);
-        list.push(cell);
-      }
-      row.cellList = list;
-      rowList.push(row);
+  createEmptyTable(col: number, row: number) {
+    this.columns = new Array<Column>();
+    for (var i = 0; i < col; i++) {
+      let col = new Column("", "");
+      this.columns.push(col);
     }
+    this.rows = new Array<Row>();
+    for (var i = 0; i < row; i++) {
+      let row = this.generateRow();
+      this.rows.push(row);
+    }
+  }
 
+  generateDataToView() {
     let table = document.createElement("div");
+    table.id = "showTable";
     let tr = document.createElement("div");
+    tr.className = "row";
 
     let idCell = document.createElement("div");
     idCell.innerHTML = "";
     idCell.className = "id";
     tr.appendChild(idCell);
 
-    for (var i = 1; i < cols.length; i++) {
+    for (var i = 0; i < this.columns.length; i++) {
       let th = document.createElement("div");
-      th.innerHTML = cols[i].name;
+      th.innerHTML = this.numberToName(i + 1);
       th.className = "head";
       tr.appendChild(th);
     }
     table.appendChild(tr);
 
-    for (var i = 0; i < rowList.length; i++) {
+    for (var i = 0; i < this.rows.length; i++) {
       let tr = document.createElement("div");
+      tr.className = "row";
 
       let idCell = document.createElement("div");
-      idCell.innerHTML = rowList[i].id.toString();
+      idCell.innerHTML = (i + 1).toString();
       idCell.className = "id";
       tr.append(idCell);
 
-      for (var j = 1; j < cols.length; j++) {
+      for (var j = 0; j < this.columns.length; j++) {
         let cell = document.createElement("div");
-        cell.innerHTML = rowList[i].getValue(cols[j].selector);
+        cell.innerHTML = this.rows[i].cellList[j].value;
         cell.className = "cell";
         tr.appendChild(cell);
       }
       table.appendChild(tr);
     }
-    var container = document.getElementById("showTable");
+    var container = document.getElementById("container");
     if (container != null) {
       container.innerHTML = "";
       container.appendChild(table);
     }
   }
+
+  numberToName(num: number) {
+    let name = "";
+    while (num >= 1) {
+      name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
+      console.log(num);
+      num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+    }
+    return name;
+  }
+
+  insertRow(index: number) {
+    let row = this.generateRow();
+    this.rows.splice(index, 0, row);
+  }
+  deleteRow(index: number) {
+    this.rows.splice(index, 1);
+  }
+  generateRow() {
+    let row = new Row(this.columns.length);
+    return row;
+  }
+
+  insertColumn(index: number) {
+    let col = new Column("", "");
+    this.columns.splice(index, 0, col);
+    for (let row of this.rows) {
+      row.insertColumn(index);
+    }
+  }
+  deleteColumn(index: number) {
+    this.columns.splice(index, 1);
+    for (let row of this.rows) {
+      row.deleteColumn(index);
+    }
+  }
+
+  // createTableFromJSON(columns: any[], data: any) {
+  //   this.columns = new Array<Column>();
+  //   for (let column of columns) {
+  //     let col = new Column(column.name, column.selector);
+  //     this.columns.push(col);
+  //   }
+
+  //   this.rows = new Array<Row>();
+  //   for (var i = 0; i < data.length; i++) {
+  //     let row = new Row(this.columns.length);
+  //     this.rows.push(row);
+  //   }
+  //   this.generateDataToView();
+  // }
 }
