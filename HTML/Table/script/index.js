fd300e3c3ce9304f376c9d80d5fdf6480c38f754
YIFAN\11466 <1146602243@qq.com>
2022-08-31 14:07:41 +0800

    加入resize，调整部分代码结构

--- HTML/Table/script/index.js
+++ HTML/Table/script/index.js
@@ -1,41 +1,131 @@
-console.log(1);
 $("#rowmenu").hide();
 $("#colmenu").hide();
-$(".head").on("contextmenu", function (e) {
-    $("#colmenu").show(100);
-    $("#colmenu").css({
-        top: e.pageY + "px",
-        left: e.pageX + "px"
+var curCol = -1;
+var curRow = -1;
+var lastFocus;
+var colresizeElement;
+var rowresizeElement;
+var press = false;
+var offsetLeft = 0;
+var offsetTop = 0;
+jQuery(function () {
+    $("#rowmenu li").on("click", function (e) {
+        if (curRow == -1) {
+            return;
+        }
+        var name = e.target.className;
+        if (name == "delete") {
+            table.deleteRow(curRow);
+        }
+        else if (name == "addbefore") {
+            table.insertRow(curRow);
+        }
+        else if (name == "addafter") {
+            table.insertRow(curRow + 1);
+        }
+        table.generateDataToView();
+        $("#rowmenu").hide();
     });
-    return false;
+    $("#colmenu li").on("click", function (e) {
+        if (curCol == -1) {
+            return;
+        }
+        var name = e.target.className;
+        if (name == "delete") {
+            table.deleteColumn(curCol);
+        }
+        else if (name == "addbefore") {
+            table.insertColumn(curCol);
+        }
+        else if (name == "addafter") {
+            table.insertColumn(curCol + 1);
+        }
+        table.generateDataToView();
+        $("#colmenu").hide();
+    });
 });
-$(".id").on("contextmenu", function (e) {
-    $("#rowmenu").show(100);
-    $("#rowmenu").css({
-        top: e.pageY + "px",
-        left: e.pageX + "px"
-    });
-    return false;
+$(document).on("contextmenu", function (event) {
+    $("#rowmenu").hide();
+    $("#colmenu").hide();
+    event.preventDefault();
 });
 $(document).on("click", function (e) {
     $("#rowmenu").hide();
     $("#colmenu").hide();
 });
-function init() {
-    var table = new Table();
-    // table.createTableFromJSON(testColumns, testData);
-    table.createEmptyTable(25, 25);
-    table.insertColumn(1);
-    table.insertColumn(4);
-    table.deleteColumn(2);
-    table.insertRow(1);
-    table.generateDataToView();
-}
+$(document).on("mouseup", function () {
+    press = false;
+    colresizeElement = undefined;
+    rowresizeElement = undefined;
+    console.log("press", press);
+});
+$(document).on("mousemove", function (e) {
+    var _a, _b;
+    console.log("press", press);
+    if (!press ||
+        (colresizeElement == undefined && rowresizeElement == undefined)) {
+        return;
+    }
+    if (colresizeElement != undefined) {
+        var width = e.pageX - Number((_a = $(colresizeElement).parent().offset()) === null || _a === void 0 ? void 0 : _a.left);
+        width = width < 100 ? 100 : width;
+        $(colresizeElement)
+            .parent()
+            .width(width + "px");
+    }
+    else if (rowresizeElement != undefined) {
+        var height = e.pageY - Number((_b = $(rowresizeElement).parent().offset()) === null || _b === void 0 ? void 0 : _b.top);
+        height = height < 30 ? 30 : height;
+        $(rowresizeElement)
+            .parent()
+            .height(height + "px");
+    }
+});
 var Column = /** @class */ (function () {
     function Column(name, selector) {
         this.name = name;
         this.selector = selector;
     }
+    Column.prototype.createColResize = function () {
+        var colresize = jQuery("<div>", {
+            "class": "colresize"
+        });
+        colresize.on("mousedown", function (e) {
+            press = true;
+            console.log("press", press);
+            offsetLeft = e.clientX;
+            colresizeElement = $(this);
+            $(this).css("cursor", "col-resize");
+        });
+        return colresize;
+    };
+    Column.prototype.createColumn = function (i) {
+        var col = document.createElement("div");
+        col.innerHTML = this.numberToName(i + 1);
+        col.className = "head";
+        $(col).on("contextmenu", function (e) {
+            curCol = $(this).index() - 1;
+            console.log("是表格的第 " + curCol + " 列");
+            $("#rowmenu").hide();
+            $("#colmenu").show(100);
+            $("#colmenu").css({
+                top: e.pageY - 10 + "px",
+                left: e.pageX + "px"
+            });
+            return false;
+        });
+        var colresize = this.createColResize();
+        colresize.appendTo(col);
+        return col;
+    };
+    Column.prototype.numberToName = function (num) {
+        var name = "";
+        while (num >= 1) {
+            name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
+            num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+        }
+        return name;
+    };
     return Column;
 }());
 var Cell = /** @class */ (function () {
@@ -43,17 +133,80 @@ var Cell = /** @class */ (function () {
         this.name = name;
         this.value = value;
     }
+    Cell.prototype.createCell = function (value) {
+        var cell = document.createElement("div");
+        $(cell).on("click", function () {
+            if (lastFocus !== undefined) {
+                console.log("test lastFocus ", lastFocus);
+                lastFocus.removeClass("focus");
+            }
+            lastFocus = $(this);
+            $(this).addClass("focus");
+        });
+        $(cell).on("dblclick", function () {
+            if (lastFocus !== undefined) {
+                lastFocus.attr("contentEditable", "false");
+            }
+            lastFocus = $(this);
+            $(this).attr("contentEditable", "true");
+        });
+        cell.innerHTML = value;
+        cell.className = "cell";
+        return cell;
+    };
     return Cell;
 }());
 var Row = /** @class */ (function () {
     function Row(count) {
         var list = new Array();
-        for (var j = 0; j < count; j++) {
+        for (var i = 0; i < count; i++) {
             var cell = new Cell("", "");
             list.push(cell);
         }
         this.cellList = list;
     }
+    Row.prototype.createIdCell = function (i) {
+        var idCell = document.createElement("div");
+        idCell.innerHTML = (i + 1).toString();
+        idCell.className = "id";
+        $(idCell).on("contextmenu", function (e) {
+            curRow = Number(e.target.innerText) - 1;
+            console.log("是表格的第 " + curRow + " 行");
+            $("#colmenu").hide();
+            $("#rowmenu").show(100);
+            $("#rowmenu").css({
+                top: e.pageY - 10 + "px",
+                left: e.pageX + "px"
+            });
+            return false;
+        });
+        var rowresize = this.createRowResize();
+        rowresize.appendTo(idCell);
+        return idCell;
+    };
+    Row.prototype.createRowResize = function () {
+        var rowresize = jQuery("<div>", {
+            "class": "rowresize"
+        });
+        rowresize.on("mousedown", function (e) {
+            press = true;
+            offsetLeft = e.clientX;
+            rowresizeElement = $(this);
+            $(this).css("cursor", "row-resize");
+        });
+        return rowresize;
+    };
+    Row.prototype.createRow = function (i) {
+        var tr = document.createElement("div");
+        tr.className = "row";
+        var idCell = this.createIdCell(i);
+        tr.append(idCell);
+        for (var j = 0; j < this.cellList.length; j++) {
+            var cell = this.cellList[j].createCell(this.cellList[j].value);
+            tr.appendChild(cell);
+        }
+        return tr;
+    };
     Row.prototype.insertColumn = function (index) {
         var cell = new Cell("", "");
         this.cellList.splice(index, 0, cell);
@@ -83,33 +236,11 @@ var Table = /** @class */ (function () {
     Table.prototype.generateDataToView = function () {
         var table = document.createElement("div");
         table.id = "showTable";
-        var tr = document.createElement("div");
-        tr.className = "row";
-        var idCell = document.createElement("div");
-        idCell.innerHTML = "";
-        idCell.className = "id";
-        tr.appendChild(idCell);
-        for (var i = 0; i < this.columns.length; i++) {
-            var th = document.createElement("div");
-            th.innerHTML = this.numberToName(i + 1);
-            th.className = "head";
-            tr.appendChild(th);
-        }
-        table.appendChild(tr);
+        var head = this.generateHead();
+        head.appendTo(table);
         for (var i = 0; i < this.rows.length; i++) {
-            var tr_1 = document.createElement("div");
-            tr_1.className = "row";
-            var idCell_1 = document.createElement("div");
-            idCell_1.innerHTML = (i + 1).toString();
-            idCell_1.className = "id";
-            tr_1.append(idCell_1);
-            for (var j = 0; j < this.columns.length; j++) {
-                var cell = document.createElement("div");
-                cell.innerHTML = this.rows[i].cellList[j].value;
-                cell.className = "cell";
-                tr_1.appendChild(cell);
-            }
-            table.appendChild(tr_1);
+            var row = this.rows[i].createRow(i);
+            table.appendChild(row);
         }
         var container = document.getElementById("container");
         if (container != null) {
@@ -117,14 +248,19 @@ var Table = /** @class */ (function () {
             container.appendChild(table);
         }
     };
-    Table.prototype.numberToName = function (num) {
-        var name = "";
-        while (num >= 1) {
-            name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
-            console.log(num);
-            num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+    Table.prototype.generateHead = function () {
+        var tr = jQuery("<div>", {
+            "class": "row"
+        });
+        var idCell = document.createElement("div");
+        idCell.innerHTML = "";
+        idCell.className = "id";
+        tr.append(idCell);
+        for (var i = 0; i < this.columns.length; i++) {
+            var col = this.columns[i].createColumn(i);
+            tr.append(col);
         }
-        return name;
+        return tr;
     };
     Table.prototype.insertRow = function (index) {
         var row = this.generateRow();
@@ -154,3 +290,8 @@ var Table = /** @class */ (function () {
     };
     return Table;
 }());
+var table = new Table();
+function init() {
+    table.createEmptyTable(25, 25);
+    table.generateDataToView();
+}
--- HTML/Table/script/index.ts
+++ HTML/Table/script/index.ts
@@ -1,22 +1,52 @@
 $("#rowmenu").hide();
 $("#colmenu").hide();
 
-$(".head").on("contextmenu", function (e) {
-  $("#colmenu").show(100);
-  $("#colmenu").css({
-    top: e.pageY + "px",
-    left: e.pageX + "px",
+let curCol = -1;
+let curRow = -1;
+let lastFocus;
+let colresizeElement;
+let rowresizeElement;
+let press = false;
+let offsetLeft = 0;
+let offsetTop = 0;
+
+jQuery(function () {
+  $("#rowmenu li").on("click", function (e) {
+    if (curRow == -1) {
+      return;
+    }
+    let name = e.target.className;
+    if (name == "delete") {
+      table.deleteRow(curRow);
+    } else if (name == "addbefore") {
+      table.insertRow(curRow);
+    } else if (name == "addafter") {
+      table.insertRow(curRow + 1);
+    }
+    table.generateDataToView();
+    $("#rowmenu").hide();
   });
-  return false;
+  $("#colmenu li").on("click", function (e) {
+    if (curCol == -1) {
+      return;
+    }
+    let name = e.target.className;
+    if (name == "delete") {
+      table.deleteColumn(curCol);
+    } else if (name == "addbefore") {
+      table.insertColumn(curCol);
+    } else if (name == "addafter") {
+      table.insertColumn(curCol + 1);
+    }
+    table.generateDataToView();
+    $("#colmenu").hide();
+  });
 });
 
-$(".id").on("contextmenu", function (e) {
-  $("#rowmenu").show(100);
-  $("#rowmenu").css({
-    top: e.pageY + "px",
-    left: e.pageX + "px",
-  });
-  return false;
+$(document).on("contextmenu", function (event) {
+  $("#rowmenu").hide();
+  $("#colmenu").hide();
+  event.preventDefault();
 });
 
 $(document).on("click", function (e) {
@@ -24,16 +54,35 @@ $(document).on("click", function (e) {
   $("#colmenu").hide();
 });
 
-function init() {
-  let table = new Table();
-  // table.createTableFromJSON(testColumns, testData);
-  table.createEmptyTable(25, 25);
-  table.insertColumn(1);
-  table.insertColumn(4);
-  table.deleteColumn(2);
-  table.insertRow(1);
-  table.generateDataToView();
-}
+$(document).on("mouseup", function () {
+  press = false;
+  colresizeElement = undefined;
+  rowresizeElement = undefined;
+  console.log("press", press);
+});
+
+$(document).on("mousemove", function (e) {
+  console.log("press", press);
+  if (
+    !press ||
+    (colresizeElement == undefined && rowresizeElement == undefined)
+  ) {
+    return;
+  }
+  if (colresizeElement != undefined) {
+    let width = e.pageX - Number($(colresizeElement).parent().offset()?.left);
+    width = width < 100 ? 100 : width;
+    $(colresizeElement)
+      .parent()
+      .width(width + "px");
+  } else if (rowresizeElement != undefined) {
+    let height = e.pageY - Number($(rowresizeElement).parent().offset()?.top);
+    height = height < 30 ? 30 : height;
+    $(rowresizeElement)
+      .parent()
+      .height(height + "px");
+  }
+});
 
 class Column {
   constructor(name: string, selector: string) {
@@ -42,6 +91,49 @@ class Column {
   }
   public name: string;
   public selector: string;
+
+  createColResize() {
+    let colresize = jQuery("<div>", {
+      class: "colresize",
+    });
+    colresize.on("mousedown", function (e) {
+      press = true;
+      console.log("press", press);
+      offsetLeft = e.clientX;
+      colresizeElement = $(this);
+      $(this).css("cursor", "col-resize");
+    });
+    return colresize;
+  }
+
+  createColumn(i: number) {
+    let col = document.createElement("div");
+    col.innerHTML = this.numberToName(i + 1);
+    col.className = "head";
+    $(col).on("contextmenu", function (e) {
+      curCol = $(this).index() - 1;
+      console.log("是表格的第 " + curCol + " 列");
+      $("#rowmenu").hide();
+      $("#colmenu").show(100);
+      $("#colmenu").css({
+        top: e.pageY - 10 + "px",
+        left: e.pageX + "px",
+      });
+      return false;
+    });
+    let colresize = this.createColResize();
+    colresize.appendTo(col);
+    return col;
+  }
+
+  numberToName(num: number) {
+    let name = "";
+    while (num >= 1) {
+      name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
+      num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+    }
+    return name;
+  }
 }
 
 class Cell {
@@ -51,22 +143,92 @@ class Cell {
   }
   public name: string;
   public value: string;
+
+  createCell(value: string) {
+    let cell = document.createElement("div");
+    $(cell).on("click", function () {
+      if (lastFocus !== undefined) {
+        console.log("test lastFocus ", lastFocus);
+        lastFocus.removeClass("focus");
+      }
+      lastFocus = $(this);
+      $(this).addClass("focus");
+    });
+    $(cell).on("dblclick", function () {
+      if (lastFocus !== undefined) {
+        lastFocus.attr("contentEditable", "false");
+      }
+      lastFocus = $(this);
+      $(this).attr("contentEditable", "true");
+    });
+    cell.innerHTML = value;
+    cell.className = "cell";
+    return cell;
+  }
 }
 
 class Row {
   constructor(count: number) {
     let list = new Array<Cell>();
-    for (var j = 0; j < count; j++) {
+    for (var i = 0; i < count; i++) {
       let cell = new Cell("", "");
       list.push(cell);
     }
     this.cellList = list;
   }
   public cellList: Cell[];
+
+  createIdCell(i: number) {
+    let idCell = document.createElement("div");
+    idCell.innerHTML = (i + 1).toString();
+    idCell.className = "id";
+    $(idCell).on("contextmenu", function (e) {
+      curRow = Number(e.target.innerText) - 1;
+      console.log("是表格的第 " + curRow + " 行");
+      $("#colmenu").hide();
+      $("#rowmenu").show(100);
+      $("#rowmenu").css({
+        top: e.pageY - 10 + "px",
+        left: e.pageX + "px",
+      });
+      return false;
+    });
+    let rowresize = this.createRowResize();
+    rowresize.appendTo(idCell);
+    return idCell;
+  }
+
+  createRowResize() {
+    let rowresize = jQuery("<div>", {
+      class: "rowresize",
+    });
+    rowresize.on("mousedown", function (e) {
+      press = true;
+      offsetLeft = e.clientX;
+      rowresizeElement = $(this);
+      $(this).css("cursor", "row-resize");
+    });
+    return rowresize;
+  }
+
+  createRow(i: number) {
+    let tr = document.createElement("div");
+    tr.className = "row";
+
+    let idCell = this.createIdCell(i);
+    tr.append(idCell);
+    for (var j = 0; j < this.cellList.length; j++) {
+      let cell = this.cellList[j].createCell(this.cellList[j].value);
+      tr.appendChild(cell);
+    }
+    return tr;
+  }
+
   insertColumn(index: number) {
     let cell = new Cell("", "");
     this.cellList.splice(index, 0, cell);
   }
+
   deleteColumn(index: number) {
     this.cellList.splice(index, 1);
   }
@@ -93,54 +255,35 @@ class Table {
   generateDataToView() {
     let table = document.createElement("div");
     table.id = "showTable";
-    let tr = document.createElement("div");
-    tr.className = "row";
+    let head = this.generateHead();
+    head.appendTo(table);
 
-    let idCell = document.createElement("div");
-    idCell.innerHTML = "";
-    idCell.className = "id";
-    tr.appendChild(idCell);
-
-    for (var i = 0; i < this.columns.length; i++) {
-      let th = document.createElement("div");
-      th.innerHTML = this.numberToName(i + 1);
-      th.className = "head";
-      tr.appendChild(th);
+    for (let i = 0; i < this.rows.length; i++) {
+      let row = this.rows[i].createRow(i);
+      table.appendChild(row);
     }
-    table.appendChild(tr);
 
-    for (var i = 0; i < this.rows.length; i++) {
-      let tr = document.createElement("div");
-      tr.className = "row";
-
-      let idCell = document.createElement("div");
-      idCell.innerHTML = (i + 1).toString();
-      idCell.className = "id";
-      tr.append(idCell);
-
-      for (var j = 0; j < this.columns.length; j++) {
-        let cell = document.createElement("div");
-        cell.innerHTML = this.rows[i].cellList[j].value;
-        cell.className = "cell";
-        tr.appendChild(cell);
-      }
-      table.appendChild(tr);
-    }
-    var container = document.getElementById("container");
+    let container = document.getElementById("container");
     if (container != null) {
       container.innerHTML = "";
       container.appendChild(table);
     }
   }
 
-  numberToName(num: number) {
-    let name = "";
-    while (num >= 1) {
-      name = String.fromCharCode(64 + (num % 26 == 0 ? 26 : num % 26)) + name;
-      console.log(num);
-      num = num % 26 == 0 ? num / 26 - 1 : num / 26;
+  generateHead() {
+    let tr = jQuery("<div>", {
+      class: "row",
+    });
+    let idCell = document.createElement("div");
+    idCell.innerHTML = "";
+    idCell.className = "id";
+    tr.append(idCell);
+
+    for (var i = 0; i < this.columns.length; i++) {
+      let col = this.columns[i].createColumn(i);
+      tr.append(col);
     }
-    return name;
+    return tr;
   }
 
   insertRow(index: number) {
@@ -168,4 +311,11 @@ class Table {
       row.deleteColumn(index);
     }
   }
+}
+
+let table = new Table();
+
+function init() {
+  table.createEmptyTable(25, 25);
+  table.generateDataToView();
 }
